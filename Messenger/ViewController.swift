14f109d6aa1bf6342fb15b42650f0095f08269ea
Vitaliy Paliy <15vitalikpaliy@gmail.com>
2019-11-02 20:51:48 -0400

    Initial Commit

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Vitaliy Paliy on 11/2/19.
+//  Copyright © 2019 PALIY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
