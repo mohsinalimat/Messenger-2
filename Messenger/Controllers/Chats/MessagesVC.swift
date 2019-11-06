7aa26c79c789a3f1b772c23abd9748a0419b301c
Vitaliy Paliy <15vitalikpaliy@gmail.com>
2019-11-06 16:38:04 -0500

    ⚙️Enables Firestore

--- Messenger/Controllers/Chats/MessagesVC.swift
+++ Messenger/Controllers/Chats/MessagesVC.swift
@@ -7,10 +7,13 @@
 //
 
 import UIKit
+import Firebase
 
 class MessagesVC: UIViewController {
-
+    
     // Message Outlets
+    
+    let db = Firestore.firestore()
     
     var texts = ["One of the most effective ways to get new users to install your app is by enabling your users to share content from your app with their friends. With Dynamic Links, you can create a great user-to-user sharing experience: users who receive content recommendations from their friends can click a link and be taken directly to the shared content in your app, even if they have to go to the App Store or Google Play Store to install your app first.One of the most effective ways to get new users to install your app is by enabling your users to share content from your app with their friends. With Dynamic Links, you can create a great user-to-user sharing experience: users who receive content recommendations from their friends can click a link and be taken directly to the shared content in your app, even if they have to go to the App Store or Google Play Store to install your app first.","Hi","What's up?"]
     
@@ -41,23 +44,33 @@ class MessagesVC: UIViewController {
     @IBAction func addButtonPressed(_ sender: Any) {
         // TODO: Add info about the user.
         print("Hi")
-    
+        
     }
     @IBAction func sendButtonPressed(_ sender: Any) {
-    
-    
-    
+        if let message = messageTextfield.text, let sender = CurrentUserInformation.email {
+            sendButton.isEnabled = true
+            sendMessagesHandler(message, sender)
+            
+        }
     }
- 
+    
+    func sendMessagesHandler(_ message: String,_ sender: String){
+        let values = ["message":message, "sender": sender]
+        db.collection("messages").addDocument(data: values) { (error) in
+            if let error = error {
+                self.showAlert(title: "Error", message: error.localizedDescription)
+                return
+            }
+            print("Data was saved")
+            
+        }
+    }
     
     @IBAction func photoLibraryPressed(_ sender: Any) {
-    
-    
-    
+        
+        
     }
-    
 }
-
 extension MessagesVC: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return texts.count
